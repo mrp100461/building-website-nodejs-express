f5fc927268636f2d25bf04a0ee3f31893fcf6131
mrp100461 <mrp100461@btinternet.com>
2021-03-01 19:51:11 +0000

    6.3 problem with feedback
    problem with posting and errosr

---
+++ routes/speakers.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const router = express.Router();
+
+module.exports = (params) => {
+  const { speakersService } = params;
+  router.get('/', async (req, res, next) => {
+    try {
+      const artwork = await speakersService.getAllArtwork();
+      const speakers = await speakersService.getList();
+      return res.render('layout', {
+        pageTitle: 'Speakers',
+        template: 'speakers',
+        speakers,
+        artwork,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  router.get('/:shortname', async (req, res, next) => {
+    try {
+      const speaker = await speakersService.getSpeaker(req.params.shortname);
+      const artwork = await speakersService.getArtworkForSpeaker(req.params.shortname);
+      return res.render('layout', {
+        pageTitle: 'Speakers',
+        template: 'speakers-detail',
+        speaker,
+        artwork,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  });
+  return router;
+};
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const cookieSession = require('cookie-session');
+const createError = require('http-errors');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const FeedbackService = require('./services/FeedbackService');
+const SpeakersService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakersService('./data/speakers.json');
+const app = express();
+
+const port = 3000;
+
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['Ghdur687399s7w', 'hhjjdf89s866799'],
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+app.locals.siteName = 'ROUX Meetups';
+
+app.use(async (req, res, next) => {
+  try {
+    const names = await speakersService.getNames();
+    res.locals.speakerNames = names;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.use('/', routes({ feedbackService, speakersService }));
+app.use((req, res, next) => next(createError(404, 'File not found')));
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  const status = err.status || 500;
+  res.locals.status = status;
+  res.status(status);
+  res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}!`);
+});
